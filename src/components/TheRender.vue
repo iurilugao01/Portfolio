d3ac9be85fb68d1dde0de09a1549a6a929d1fbb6
iurilugao01 <iurilugao01@gmail.com>
2025-06-09 14:53:00 -0300

    first commit

---
+++ src/components/TheRender.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<RenderProps>(), {
+  color: "white",
+  icon: "none",
+});
+</script>
+
+<template></template>
---
+++ src/types/interfaces.ts
@@ -0,0 +1,8 @@
+interface RenderProps {
+  link: string;
+  content: string;
+  color?: string;
+  icon?: string;
+}
+
+export type { RenderProps };
